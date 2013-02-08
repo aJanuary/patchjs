6b3f56e9a50524c7592525d12d908b72a89699b1
Andrew January <andrewjanuary@gmail.com>
2013-02-08 18:38:19 +0000

    Patch can now patch multiple properies at once.

--- spec/patch.spec.js
+++ spec/patch.spec.js
@@ -18,7 +18,7 @@ describe('Patch', function() {
     it('invokes the scope function', function() {
 	    var hasBeenCalled = false;
 
-	    Patch.object({}, 'prop', null, function() {
+	    Patch.object({}, {}, function() {
 	      hasBeenCalled = true;
 	    });
 
@@ -26,31 +26,38 @@ describe('Patch', function() {
 	  });
 	
     it('patches functions', function() {
-      Patch.object(obj, 'isPatched', function() { return true; }, function() {
+      Patch.object(obj, {'isPatched': function() { return true; }}, function() {
 	      expect(obj.isPatched()).toBe(true);
 	    });
     });
     
     it('patches variables', function() {
-      Patch.object(obj, 'patched', true, function() {
+      Patch.object(obj, {'patched': true}, function() {
 	      expect(obj.patched).toBe(true);
 	    });
     });
     
+    it('patches multiple properties', function() {
+      Patch.object(obj, {'isPatched': function() { return true; }, 'arguments': [1, 2, 3]}, function() {
+        expect(obj.isPatched()).toBe(true);
+        expect(obj.arguments).toEqual([1, 2, 3]);
+      });
+    });
+    
     it('creates a patch function out of a constant value', function() {
-      Patch.object(obj, 'isPatched', true, function() {
+      Patch.object(obj, {'isPatched': true}, function() {
 	      expect(obj.isPatched()).toBe(true);
 	    });
     });
 	
   	it('unpatches after executing the scope', function() {
-      Patch.object(obj, 'isPatched', function() { return true; }, function() { });
+      Patch.object(obj, {'isPatched': function() { return true; }}, function() { });
 	    expect(obj.isPatched()).toBe(false);
 	  });
 	
     it('unpatches if scope throws an exception', function() {
       expect(function() {
-        Patch.object(obj, 'isPatched', function() { return true; }, function() {
+        Patch.object(obj, {'isPatched': function() { return true; }}, function() {
           throw 'error';
         })
       }).toThrow('error');
@@ -63,7 +70,7 @@ describe('Patch', function() {
     it('invokes the scope function', function() {
       var hasBeenCalled = false;
       
-      Patch.new_objects(this, 'Obj', 'prop', null, function() {
+      Patch.new_objects(Namespace, 'Obj', {}, function() {
         hasBeenCalled = true;
       });
       
@@ -71,31 +78,39 @@ describe('Patch', function() {
     });
     
     it('patches functions', function() {
-      Patch.new_objects(Namespace, 'Obj', 'isPatched', function() { return true; }, function() {
+      Patch.new_objects(Namespace, 'Obj', {'isPatched': function() { return true; }}, function() {
         expect(new Namespace.Obj().isPatched()).toBe(true);
       });
     });
     
     it('patches variables', function() {
-      Patch.new_objects(Namespace, 'Obj', 'patched', true, function() {
+      Patch.new_objects(Namespace, 'Obj', {'patched': true}, function() {
         expect(new Namespace.Obj().patched).toBe(true);
       });
     });
     
+    it('patches multiple properties', function() {
+      Patch.new_objects(Namespace, 'Obj', {'isPatched': true, 'arguments': [1, 2, 3]}, function() {
+        var newObj = new Namespace.Obj();
+        expect(newObj.isPatched()).toBe(true);
+        expect(newObj.arguments).toEqual([1, 2, 3]);
+      });
+    });
+    
     it('creates a patch function out of a constant value', function() {
-      Patch.new_objects(Namespace, 'Obj', 'isPatched', true, function() {
+      Patch.new_objects(Namespace, 'Obj', {'isPatched': true}, function() {
         expect(new Namespace.Obj().isPatched()).toBe(true);
       });
     });
     
     it('unpatches after executing the scope', function() {
-      Patch.new_objects(Namespace, 'Obj', 'isPatched', true, function() { });
+      Patch.new_objects(Namespace, 'Obj', {'isPatched': true}, function() { });
       expect(new Namespace.Obj().isPatched()).toBe(false);
     });
     
     it('unpatches if scope throws an exception', function() {
       expect(function() {
-        Patch.new_objects(Namespace, 'Obj', 'isPatched', true, function() {
+        Patch.new_objects(Namespace, 'Obj', {'isPatched': true}, function() {
           throw 'error';
         })
       }).toThrow('error');
@@ -104,14 +119,14 @@ describe('Patch', function() {
     });
     
     it('passes arguments to original constructor', function() {
-      Patch.new_objects(Namespace, 'Obj', 'isPatched', true, function() {
+      Patch.new_objects(Namespace, 'Obj', {'isPatched': true}, function() {
         var newObj = new Namespace.Obj(1, 2, 3);
         expect(newObj.arguments).toEqual([1, 2, 3]);
       });
     });
     
     it('does not affect existing objects', function() {
-      Patch.new_objects(Namespace, 'Obj', 'isPatched', true, function() {
+      Patch.new_objects(Namespace, 'Obj', {'isPatched': true}, function() {
         expect(obj.isPatched()).toBe(false);
       });
     });
--- src/patch.js
+++ src/patch.js
@@ -1,15 +1,33 @@
 Patch = (function() {
-  function apply_patch(obj, prop_name, new_val) {
-    var old_val = obj[prop_name];
+  function apply_patch(obj, patches) {
+    var applied = [];
+    
+    for (var prop_name in patches) {
+      if (!patches.hasOwnProperty(prop_name)) { continue; }
 
-    if (typeof old_val === 'function' && typeof new_val !== 'function') {
-      var const_val = new_val;
-      new_val = function() { return const_val; }
+      var old_val = obj[prop_name];
+      var new_val = patches[prop_name];
+
+      if (typeof old_val === 'function' && typeof new_val !== 'function') {
+        var const_val = new_val;
+        new_val = function() { return const_val; }
+      }
+      
+      obj[prop_name] = new_val;
+      applied.push({
+        prop_name: prop_name,
+        old_val: old_val
+      });
     }
     
-	  obj[prop_name] = new_val;
-    
-    return old_val;
+    return applied;
+  }
+  
+  function revert_patches(obj, patches) {
+    for (var i = 0, len = patches.length; i < len; i += 1) {
+      var patch = patches[i];
+      obj[patch.prop_name] = patch.old_val;
+    }
   }
   
    function construct_object(Constructor, args) {
@@ -22,21 +40,21 @@ Patch = (function() {
 		return Object(ret) === ret ? ret : inst;
 	}
   
-  function patch_object(obj, prop_name, new_val, scope) {
-    var old_val = apply_patch(obj, prop_name, new_val);
+  function patch_object(obj, patches, scope) {
+    var applied_patches = apply_patch(obj, patches);
 
     try {
       scope();
     } finally {
-      obj[prop_name] = old_val;
+      revert_patches(obj, applied_patches);
 	  }
   }
   
-  function patch_new_objects(context, obj_name, prop_name, new_val, scope) {
+  function patch_new_objects(context, obj_name, patches, scope) {
     var OldConstructor = context[obj_name];
     context[obj_name] = function() {
       var obj = new construct_object(OldConstructor, arguments);
-      apply_patch(obj, prop_name, new_val);
+      apply_patch(obj, patches);
       return obj;
     };
     
