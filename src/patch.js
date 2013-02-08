a2c86318720e217172a05420b740f5fc4ec6ecd8
Andrew January <andrewjanuary@gmail.com>
2013-02-08 18:53:05 +0000

    Refactors patch code.

--- src/patch.js
+++ src/patch.js
@@ -1,26 +1,40 @@
 Patch = (function() {
-  function apply_patch(obj, patches) {
+  function apply_patches(obj, patches) {
     var applied = [];
     
     for (var prop_name in patches) {
       if (!patches.hasOwnProperty(prop_name)) { continue; }
-
-      var old_val = obj[prop_name];
-      var new_val = patches[prop_name];
-
-      if (typeof old_val === 'function' && typeof new_val !== 'function') {
-        var const_val = new_val;
-        new_val = function() { return const_val; }
-      }
       
-      obj[prop_name] = new_val;
-      applied.push({
-        prop_name: prop_name,
-        old_val: old_val
-      });
+      var new_val = patches[prop_name];
+      var patch = apply_patch(obj, prop_name, new_val);
+      applied.push(patch);
     }
     
     return applied;
+  }
+  
+  function apply_patch(obj, prop_name, new_val) {
+    var old_val = obj[prop_name];
+    new_val = const_to_function(new_val, old_val);
+      
+    obj[prop_name] = new_val;
+      
+    return {
+      prop_name: prop_name,
+      old_val: old_val
+    }
+  }
+  
+  function const_to_function(new_val, old_val) {
+    var expecting_function = typeof old_val === 'function';
+    var new_val_is_const = typeof new_val !== 'function';
+
+    if (expecting_function && new_val_is_const) {
+      var const_val = new_val;
+      return function() { return const_val; };
+    } else {
+      return new_val;
+    }
   }
   
   function revert_patches(obj, patches) {
@@ -41,7 +55,7 @@ Patch = (function() {
 	}
   
   function patch_object(obj, patches, scope) {
-    var applied_patches = apply_patch(obj, patches);
+    var applied_patches = apply_patches(obj, patches);
 
     try {
       scope();
@@ -54,7 +68,7 @@ Patch = (function() {
     var OldConstructor = context[obj_name];
     context[obj_name] = function() {
       var obj = new construct_object(OldConstructor, arguments);
-      apply_patch(obj, patches);
+      apply_patches(obj, patches);
       return obj;
     };
     
